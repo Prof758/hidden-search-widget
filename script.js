af437f7d761fca9ce86d166916b7906fc0e8c565
Simon Prophet <simon.s.prophet@outlook.com>
2023-05-11 12:10:12 +0100

    add JS code to page and border radius to input and btn

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 'use strict';
+const btn = document.querySelector('.btn');
+
+const search = document.querySelector('.search');
+
+const input = document.querySelector('.input');
+
+btn.addEventListener('click', () => {
+  search.classList.toggle('active');
+  input.focus();
+});
